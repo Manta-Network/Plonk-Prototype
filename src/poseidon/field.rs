cec61b5909eb8b113b2fa81c6392a672df9de533
Tom Shen <tomshen@berkeley.edu>
2022-01-10 14:56:12 -0800

    circuits for poseidon_ref

--- src/poseidon/field.rs
+++ src/poseidon/field.rs
@@ -1,46 +1,77 @@
 //! Library independent specification for field trait.
 //! `COM` is `()` when the field is in native, and is constraint synthesizer
 //! when the field is a variable.
+use std::fmt::Debug;
 use ark_ec::{PairingEngine, TEModelParameters};
 use ark_ff::PrimeField;
 use ark_plonk::prelude::*;
-use num_traits::One;
+use num_traits::{One, Zero};
 use std::marker::PhantomData;
 
-pub trait COMArith<COM = ()>: Sized {
+pub trait COMArith<COM = ()>: Sized + Clone + Debug {
+    type Native: COMArith<()> + COMFromInt<()>;
+    type PublicInput;
     // I added `com_` prefix here to avoid conflict with num_traits. Any suggestion
     // is welcome!
     /// additive identity
     fn com_zero(c: &mut COM) -> Self;
+    fn com_zero_native() -> Self::Native{
+        <Self::Native as COMArith<()>>::com_zero(&mut ())
+    }
+    fn com_one_native() -> Self::Native{
+        <Self::Native as COMFromInt<()>>::com_one(&mut ())
+    }
+    fn com_native_from_int(v: u64) -> Self::Native {
+        <Self::Native as COMFromInt<()>>::com_from_const_int(&mut (), v)
+    }
+    fn com_alloc(c: &mut COM, v: Self::Native) -> Self;
+    fn zeros<const SIZE: usize>(c: &mut COM) -> [Self; SIZE];
     /// add two field elements
-    fn com_add(&self, b: &Self, c: &mut COM) -> Self;
-    /// multiply field element by `-1`
+    fn com_add(&self, c: &mut COM, b: &Self) -> Self;
+    /// add a constant
+    fn com_add_const(&self, c: &mut COM, b: &Self::Native) -> Self;
+    /// the additive inverse of a field element
     fn com_neg(&self, c: &mut COM) -> Self;
-    /// subtract two field elements
-    fn com_mul(&self, other: &Self, c: &mut COM) -> Self;
-    fn com_add_assign(&mut self, other: &Self, c: &mut COM) {
-        *self = self.com_add(other, c);
+    /// multiply two field elements
+    fn com_mul(&self, c: &mut COM, other: &Self) -> Self;
+    /// multiply a field element by a constant
+    fn com_mul_const(&self, c: &mut COM, other: &Self::Native) -> Self;
+    fn com_square(&self, c: &mut COM) -> Self {
+        self.com_mul(c, self)
+    }
+    fn com_add_assign(&mut self, c: &mut COM, other: &Self) {
+        *self = self.com_add(c, other);
+    }
+    fn com_mul_assign(&mut self, c: &mut COM, other: &Self) {
+        *self = self.com_mul(c, other);
+    }
+    fn com_add_assign_const(&mut self, c: &mut COM, other: &Self::Native) {
+        *self = self.com_add_const(c, other);
+    }
+    fn com_mul_assign_const(&mut self, c: &mut COM, other: &Self::Native) {
+        *self = self.com_mul_const(c, other);
     }
 }
 
-pub trait COMOne<COM = ()>: COMArith<()> + Sized {
+pub trait COMFromInt<COM = ()>: COMArith<()> + Sized {
     /// multiplicative identity
     fn com_one(c: &mut COM) -> Self;
+    fn com_from_const_int(c: &mut COM, v: u64) -> Self;
 }
 
 pub trait COMPower<COM = ()>: COMArith<()> + Sized {
     type Scalar;
-    fn com_pow(&self, exp: &Self::Scalar, c: &mut COM) -> Self;
+    fn com_pow(&self, c: &mut COM, exp: &Self::Scalar) -> Self;
 }
 
 pub trait COMArithExt<COM = ()>: COMArith<COM> + Sized {
-    type Native: COMArith<()> + COMOne<()>;
-    type PublicInput;
     fn __make_arith_gate(c: &mut COM, config: ArithExtBuilder<Self, COM>) -> Self;
     fn com_arith(c: &mut COM) -> ArithExtBuilder<Self, COM> {
         ArithExtBuilder::new(c)
     }
 }
+
+pub type NativeField<S, COM> = <S as COMArith<COM>>::Native;
 
 /// `(w_l * w_r) * q_m + a * q_l + b * q_r + w_4 * q_4 + q_c + PI + q_o * c = 0`
 /// where output is `c`
@@ -124,11 +155,25 @@ impl<F: COMArithExt<COM>, COM> ArithExtBuilder<F, COM> {
 }
 
 impl<F: PrimeField> COMArith<()> for F {
+    type Native = F;
+    type PublicInput = F;
     fn com_zero(_c: &mut ()) -> Self {
         F::zero()
     }
 
-    fn com_add(&self, b: &Self, _c: &mut ()) -> Self {
+    fn com_alloc(_c: &mut (), v: Self::Native) -> Self {
+        v
+    }
+
+    fn zeros<const SIZE: usize>(_c: &mut ()) -> [Self; SIZE] {
+        [F::zero(); SIZE]
+    }
+
+    fn com_add(&self, _c: &mut (), b: &Self) -> Self {
+        *self + *b
+    }
+
+    fn com_add_const(&self, _c: &mut (), b: &Self::Native) -> Self {
         *self + *b
     }
 
@@ -136,47 +181,26 @@ impl<F: PrimeField> COMArith<()> for F {
         -*self
     }
 
-    fn com_mul(&self, other: &Self, _c: &mut ()) -> Self {
+    fn com_mul(&self, _c: &mut (), other: &Self) -> Self {
+        *self * *other
+    }
+
+    fn com_mul_const(&self, _c: &mut (), other: &Self::Native) -> Self {
         *self * *other
     }
 }
 
-impl<F: PrimeField> COMOne<()> for F {
+impl<F: PrimeField> COMFromInt<()> for F {
     fn com_one(_c: &mut ()) -> Self {
         F::one()
     }
-}
 
-impl<E, P> COMArith<StandardComposer<E, P>> for Variable
-where
-    E: PairingEngine,
-    P: TEModelParameters<BaseField = E::Fr>,
-{
-    fn com_zero(c: &mut StandardComposer<E, P>) -> Self {
-        c.zero_var()
-    }
-
-    fn com_add(&self, b: &Self, c: &mut StandardComposer<E, P>) -> Self {
-        c.arithmetic_gate(|g| g.witness(*self, *b, None))
-    }
-
-    fn com_neg(&self, c: &mut StandardComposer<E, P>) -> Self {
-        let zero = c.zero_var();
-        c.arithmetic_gate(|g| {
-            g.witness(*self, zero, None)
-                .add(E::Fr::one(), E::Fr::from(0u64))
-        })
-    }
-
-    fn com_mul(&self, other: &Self, c: &mut StandardComposer<E, P>) -> Self {
-        c.arithmetic_gate(|g| g.witness(*self, *other, None).mul(E::Fr::one()))
+    fn com_from_const_int(c: &mut (), v: u64) -> Self {
+        F::from(v)
     }
 }
 
 impl<F: PrimeField> COMArithExt<()> for F {
-    type Native = F;
-    type PublicInput = F;
-
     fn __make_arith_gate(_c: &mut (), config: ArithExtBuilder<Self, ()>) -> Self {
         let mut result = F::zero();
         result += (config.w_l * config.w_r) * config.q_m;
@@ -192,7 +216,7 @@ impl<F: PrimeField> COMArithExt<()> for F {
     }
 }
 
-impl<E, P> COMArithExt<StandardComposer<E, P>> for Variable
+impl<E, P> COMArith<StandardComposer<E, P>> for Variable
 where
     E: PairingEngine,
     P: TEModelParameters<BaseField = E::Fr>,
@@ -200,6 +224,55 @@ where
     type Native = E::Fr;
     type PublicInput = E::Fr;
 
+    fn com_zero(c: &mut StandardComposer<E, P>) -> Self {
+        c.zero_var()
+    }
+
+    fn com_alloc(c: &mut StandardComposer<E, P>, v: Self::Native) -> Self {
+        c.add_input(v)
+    }
+
+    fn zeros<const SIZE: usize>(c: &mut StandardComposer<E, P>) -> [Self; SIZE] {
+        [c.zero_var(); SIZE]
+    }
+
+
+    fn com_add(&self, c: &mut StandardComposer<E, P>, b: &Self) -> Self {
+        c.arithmetic_gate(|g| g.witness(*self, *b, None).add(E::Fr::one(), E::Fr::one()))
+    }
+
+    fn com_add_const(&self, c: &mut StandardComposer<E, P>, b: &Self::Native) -> Self {
+        let zero = c.zero_var();
+        c.arithmetic_gate(|g| {
+            g.witness(*self, zero, None)
+                .add(E::Fr::one(), E::Fr::zero())
+                .constant(*b)
+        })
+    }
+
+    fn com_neg(&self, c: &mut StandardComposer<E, P>) -> Self {
+        let zero = c.zero_var();
+        c.arithmetic_gate(|g| g.witness(*self, zero, None).out(E::Fr::one()))
+    }
+
+    fn com_mul(&self, c: &mut StandardComposer<E, P>, other: &Self) -> Self {
+        c.arithmetic_gate(|g| g.witness(*self, *other, None).mul(E::Fr::one()))
+    }
+
+    fn com_mul_const(&self, c: &mut StandardComposer<E, P>, other: &Self::Native) -> Self {
+        let zero = c.zero_var();
+        c.arithmetic_gate(|g| {
+            g.witness(*self, zero, None)
+                .add(*other, E::Fr::zero())
+        })
+    }
+}
+
+impl<E, P> COMArithExt<StandardComposer<E, P>> for Variable
+where
+    E: PairingEngine,
+    P: TEModelParameters<BaseField = E::Fr>,
+{
     fn __make_arith_gate(
         c: &mut StandardComposer<E, P>,
         config: ArithExtBuilder<Self, StandardComposer<E, P>>,
--- src/poseidon/poseidon_ref.rs
+++ src/poseidon/poseidon_ref.rs
@@ -1,11 +1,15 @@
 //! Correct, Naive, reference implementation of Poseidon hash function.
 
+use crate::poseidon::field::{COMArith, COMArithExt, NativeField};
 use crate::poseidon::{
     mds::MdsMatrices, round_constant::generate_constants, round_numbers::calc_round_numbers,
     PoseidonError,
 };
 use ark_ff::PrimeField;
 use std::convert::TryInto;
+use std::marker::PhantomData;
+use ark_ec::{PairingEngine, TEModelParameters};
+use ark_plonk::prelude::{StandardComposer, Variable};
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct PoseidonConstants<F: PrimeField> {
@@ -49,19 +53,67 @@ impl<F: PrimeField> PoseidonConstants<F> {
     }
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct Poseidon<F: PrimeField, const WIDTH: usize> {
-    pub(crate) constants_offset: usize,
-    pub(crate) current_round: usize,
-    pub elements: [F; WIDTH],
-    pos: usize,
-    pub(crate) constants: PoseidonConstants<F>,
+pub trait PoseidonSpec<COM = ()> {
+    type Field: COMArithExt<COM>;
+    /// return (x + pre_add)^5 + post_add
+    // todo: for PLONK, we can have some specialization for s_box
+    fn quintic_s_box(
+        c: &mut COM,
+        x: Self::Field,
+        pre_add: Option<NativeField<Self::Field, COM>>,
+        post_add: Option<NativeField<Self::Field, COM>>,
+    ) -> Self::Field {
+        let mut tmp = match pre_add {
+            Some(a) => x.com_add_const(c, &a),
+            None => x.clone(),
+        };
+        tmp = tmp.com_square(c);
+        tmp = tmp.com_square(c);
+        tmp.com_mul_assign(c, &x);
+        match post_add {
+            Some(a) => tmp.com_add_const(c, &a),
+            None => tmp,
+        }
+    }
 }
 
-impl<F: PrimeField, const WIDTH: usize> Poseidon<F, WIDTH> {
-    pub fn new(constants: PoseidonConstants<F>) -> Self {
-        let mut elements = [F::zero(); WIDTH];
-        elements[0] = constants.domain_tag;
+pub struct PoseidonRefNativeSpec<F: PrimeField> {
+    pub _field: PhantomData<F>,
+}
+
+pub struct PoseidonRefPlonkSpec{}
+
+impl<F: PrimeField> PoseidonSpec for PoseidonRefNativeSpec<F> {
+    type Field = F;
+}
+
+impl<E, P> PoseidonSpec<StandardComposer<E, P>> for PoseidonRefPlonkSpec
+    where
+        E: PairingEngine,
+        P: TEModelParameters<BaseField = E::Fr>,
+{
+    type Field = Variable;
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Poseidon<COM, S: PoseidonSpec<COM>, const WIDTH: usize>
+where
+    NativeField<S::Field, COM>: PrimeField, // TODO: for now, we only support arkwork's native field. After refactoring PoseidonConstants, we can support other field libraries!
+{
+    pub(crate) constants_offset: usize,
+    pub(crate) current_round: usize,
+    pub elements: [S::Field; WIDTH],
+    pos: usize,
+    pub(crate) constants: PoseidonConstants<NativeField<S::Field, COM>>,
+}
+
+impl<COM, S: PoseidonSpec<COM>, const WIDTH: usize> Poseidon<COM, S, WIDTH>
+where
+    NativeField<S::Field, COM>: PrimeField,
+{
+    pub fn new(c: &mut COM, constants: PoseidonConstants<NativeField<S::Field, COM>>) -> Self {
+        let mut elements =  S::Field::zeros(c);
+        elements[0] = S::Field::com_alloc(c, constants.domain_tag);
         Poseidon {
             constants_offset: 0,
             current_round: 0,
@@ -75,19 +127,19 @@ impl<F: PrimeField, const WIDTH: usize> Poseidon<F, WIDTH> {
         WIDTH - 1
     }
 
-    pub fn reset(&mut self) {
+    pub fn reset(&mut self, c: &mut COM) {
         self.constants_offset = 0;
         self.current_round = 0;
         self.elements[1..]
             .iter_mut()
-            .for_each(|l| *l = F::from(0u64));
-        self.elements[0] = self.constants.domain_tag;
+            .for_each(|l| *l = S::Field::com_zero(c));
+        self.elements[0] = S::Field::com_alloc(c, self.constants.domain_tag);
         self.pos = 1;
     }
 
     /// input one field element to Poseidon. Return the position of the element
     /// in state.
-    pub fn input(&mut self, input: F) -> Result<usize, PoseidonError> {
+    pub fn input(&mut self, input: S::Field) -> Result<usize, PoseidonError> {
         // Cannot input more elements than the defined constant width
         if self.pos >= WIDTH {
             return Err(PoseidonError::FullBuffer);
@@ -101,23 +153,23 @@ impl<F: PrimeField, const WIDTH: usize> Poseidon<F, WIDTH> {
     }
 
     /// Output the hash
-    pub fn output_hash(&mut self) -> F {
+    pub fn output_hash(&mut self, c: &mut COM) -> S::Field {
         for _ in 0..self.constants.half_full_rounds {
-            self.full_round();
+            self.full_round(c);
         }
 
         for _ in 0..self.constants.partial_rounds {
-            self.partial_round();
+            self.partial_round(c);
         }
 
         for _ in 0..self.constants.half_full_rounds {
-            self.full_round();
+            self.full_round(c);
         }
 
-        self.elements[1]
+        self.elements[1].clone()
     }
 
-    fn full_round(&mut self) {
+    fn full_round(&mut self, c: &mut COM) {
         let pre_round_keys = self
             .constants
             .round_constants
@@ -129,64 +181,57 @@ impl<F: PrimeField, const WIDTH: usize> Poseidon<F, WIDTH> {
             .iter_mut()
             .zip(pre_round_keys)
             .for_each(|(l, pre)| {
-                *l = quintic_s_box(*l, pre.map(|x| *x), None);
+                *l = S::quintic_s_box(c, l.clone(), pre.map(|x| *x), None);
             });
 
         self.constants_offset += self.elements.len();
 
-        self.product_mds();
+        self.product_mds(c);
     }
 
-    fn partial_round(&mut self) {
-        self.add_round_constants();
+    fn partial_round(&mut self, c: &mut COM) {
+        self.add_round_constants(c);
 
         // apply quintic s-box to the first element
-        self.elements[0] = quintic_s_box(self.elements[0], None, None);
+        self.elements[0] = S::quintic_s_box(c, self.elements[0].clone(), None, None);
 
         // Multiply by MDS
-        self.product_mds();
+        self.product_mds(c);
     }
 
-    fn add_round_constants(&mut self) {
+    fn add_round_constants(&mut self, c: &mut COM) {
         for (element, round_constant) in self
             .elements
             .iter_mut()
             .zip(self.constants.round_constants.iter())
             .skip(self.constants_offset)
         {
-            *element += round_constant;
+            element.com_add_const(c, round_constant);
         }
 
         self.constants_offset += self.elements.len();
     }
 
     /// Multiply current state by MDS matrix
-    fn product_mds(&mut self) {
+    fn product_mds(&mut self, c: &mut COM) {
         let matrix = &self.constants.mds_matrices.m;
-        let mut result = [F::zero(); WIDTH];
+        let mut result = S::Field::zeros::<WIDTH>(c);
 
         for (j, val) in result.iter_mut().enumerate() {
             for (i, row) in matrix.iter_rows().enumerate() {
-                *val += row[j] * self.elements[i];
+                // TODO: shall we move this to spec
+                // *val += row[j] * self.elements[i]
+                let updated_val = S::Field::com_arith(c)
+                    .w_l(self.elements[i].clone())
+                    .q_l(row[j]) // row[j] * self.elements[i]
+                    .w_r(val.clone())
+                    .q_r(S::Field::com_one_native())// *val
+                    .build(c);
+                *val = updated_val;
             }
         }
 
         self.elements = result;
-    }
-}
-
-/// return (x + pre_add)^5 + post_add
-fn quintic_s_box<F: PrimeField>(x: F, pre_add: Option<F>, post_add: Option<F>) -> F {
-    let mut c: F = match pre_add {
-        Some(a) => x + a,
-        None => x,
-    };
-    c = c.square();
-    c = c.square();
-    c *= x;
-    match post_add {
-        Some(a) => c + a,
-        None => c,
     }
 }
 
@@ -204,11 +249,11 @@ mod tests {
         let mut rng = test_rng();
 
         let param = PoseidonConstants::generate::<WIDTH>();
-        let mut poseidon = Poseidon::<Fr, WIDTH>::new(param);
+        let mut poseidon = Poseidon::<(), PoseidonRefNativeSpec<Fr>, WIDTH>::new(&mut (), param);
         (0..ARITY).for_each(|_| {
             let _ = poseidon.input(Fr::rand(&mut rng)).unwrap();
         });
-        let _ = poseidon.output_hash();
+        let _ = poseidon.output_hash(&mut ());
     }
 
     #[test]
@@ -220,10 +265,10 @@ mod tests {
         let mut rng = test_rng();
 
         let param = PoseidonConstants::generate::<WIDTH>();
-        let mut poseidon = Poseidon::<Fr, WIDTH>::new(param);
+        let mut poseidon = Poseidon::<(), PoseidonRefNativeSpec<Fr>, WIDTH>::new(&mut (), param);
         (0..(ARITY + 1)).for_each(|_| {
             let _ = poseidon.input(Fr::rand(&mut rng)).unwrap();
         });
-        let _ = poseidon.output_hash();
+        let _ = poseidon.output_hash(&mut ());
     }
 }
